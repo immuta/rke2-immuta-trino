e4df9edf04c6236914c5701e008cf5a737691b41
Ryan Bauman <rbauman@immuta.com>
2022-07-13 14:30:58 -0400

    Update versions

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo yum install -y python3
 pip3 install --user awscli
 mkdir immuta
 pushd immuta
-aws s3 cp s3://immuta-rke2/immuta-2022.1.3-trino-387-rke2-v1.23.7.tar.gz .
-tar xf immuta-2022.1.3-trino-387-rke2-v1.23.7.tar.gz
+aws s3 cp s3://immuta-rke2/immuta-2022.2.0-trino-389-rke2-v1.23.8.tar.gz .
+tar xf immuta-2022.2.0-trino-389-rke2-v1.23.8.tar.gz
 python3 -m venv .venv
 . .venv/bin/activate
 pip3 install --upgrade pip
